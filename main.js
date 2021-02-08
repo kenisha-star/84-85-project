35c8821fb747742189d8b9727ea0cbfb97121236
kenisha-star <77574261+kenisha-star@users.noreply.github.com>
2021-02-08 16:20:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 canvas= document.getElementById("myCanvas");
 ctx= canvas.getContext("2d");
 r_w=100,r_h=90,r_x=100,r_y=200;
-b_image="mars.jpg",r_image="rover.png";
+b_image="racing.jpg",r_image="car1.png";
 function picture() {
  b=new Image();
  b.onload=upbg;
@@ -16,4 +16,5 @@ ctx.drawImage(b,0,0,canvas.width,canvas.height);
 }
 function uprover() {
 ctx.drawImage(r,r_x,r_y,r_w,r_h);    
+}
 }
